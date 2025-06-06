76c2e2df9f7bec8fa0d7bb251e8ca5b64984782e
Cindy <cindy870709@gmail.com>
2025-06-06 13:10:35 +0800

    update ip saving strategy

--- repositories/ip.py
+++ repositories/ip.py
@@ -2,16 +2,39 @@ from sqlalchemy.orm import Session
 from database.model import IpLocation
 from fastapi import HTTPException
 from sqlalchemy.exc import IntegrityError
+from sqlalchemy import text
+from pydantic import BaseModel
 
 
-def save_geo_to_db(db: Session, geo_info):
+class IpInfor(BaseModel):
+    ip: str
+    country: str | None
+    city: str | None
+    latitude: str | None
+    longitude: str | None
+
+
+def save_geo_to_db(db: Session, geo_info: IpInfor):
     try:
-        if geo_info:
-            new_ip_info = IpLocation(**geo_info)
-            db.add(new_ip_info)
-            db.commit()
-    except IntegrityError as e:
-        db.rollback()
-        print("此ip已存在，不需再儲存")
+        if not geo_info or not geo_info.ip:
+            raise HTTPException(status_code=400, detail="找不到 IP info")
+
+        stmt = text("""
+            INSERT INTO ip_location (ip_address, country, city, latitude, longitude)
+            VALUES (:ip, :country, :city, :lat, :lon)
+            ON DUPLICATE KEY UPDATE
+                country = VALUES(country),
+                city = VALUES(city),
+                latitude = VALUES(latitude),
+                longitude = VALUES(longitude)
+        """)
+        db.execute(stmt, {
+            "ip": geo_info.ip,
+            "country": geo_info.country,
+            "city": geo_info.city,
+            "lat": geo_info.latitude,
+            "lon": geo_info.longitude
+        })
+        db.commit()
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
